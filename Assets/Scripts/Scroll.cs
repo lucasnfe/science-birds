faa9edb0579f27ba41e2635dd65d728a07d33da5
Lucas Ferreira <lucasnfe@gmail.com>
2014-03-21 00:21:52 -0300

    Reduced textures/image scale and implemented a simple parallax

--- Assets/Scripts/Scroll.cs
+++ Assets/Scripts/Scroll.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class Scroll : MonoBehaviour {
 
 	public float mouseSensitivity = 1.0f;
-	public float maxDistanceRight = 1.0f;
 	public float maxDistanceLeft = 1.0f;
+	public float maxDistanceRight = 1.0f;
+	
+	public Rigidbody2D[] backgroundLayers;
 	
 	private bool didTouchBackground = false;
 	
@@ -38,12 +40,34 @@ public class Scroll : MonoBehaviour {
 			{
 		        Vector3 deltaMovement = Input.mousePosition - lastPosition;
 				deltaMovement = new Vector3(Mathf.Clamp(deltaMovement.x, -30, 30), deltaMovement.y, 0);
-						
-				rigidbody2D.AddForce(new Vector2(deltaMovement.x * mouseSensitivity * Time.deltaTime, 0));
+				
+				float currentAppliedForce = deltaMovement.x * mouseSensitivity * Time.deltaTime;		
+				rigidbody2D.AddForce(new Vector2(currentAppliedForce, 0));
 	        
+				foreach(Rigidbody2D layer in backgroundLayers)
+				{
+					currentAppliedForce *= 0.75f;
+					layer.AddForce(new Vector2(currentAppliedForce, 0));
+				}
+			
 				lastPosition = Input.mousePosition;
 			}
 	    }
+		
+		float currentmaxDistanceLeft = maxDistanceLeft;
+		float currentmaxDistanceRight = maxDistanceRight;
+		
+		foreach(Rigidbody2D layer in backgroundLayers)
+		{
+			currentmaxDistanceLeft *= 0.75f;
+			currentmaxDistanceRight *= 0.75f;
+			
+			layer.transform.position = new Vector3(Mathf.Clamp(layer.transform.position.x, initialPosition.x - currentmaxDistanceLeft, 
+																		   initialPosition.x + currentmaxDistanceRight), layer.transform.position.y, layer.transform.position.z); 
+			
+			
+		}
+	
 		
 		transform.position = new Vector3(Mathf.Clamp(transform.position.x, initialPosition.x - maxDistanceLeft, 
 																		   initialPosition.x + maxDistanceRight), transform.position.y, transform.position.z); 
