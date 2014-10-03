8cdc08a70e497a99c36098c6e0c585e3d3ec402b
Lucas Ferreira <lucasnfe@gmail.com>
2014-10-03 06:58:40 -0300

    Restart level when all pigs are killed

---
+++ Assets/Scripts/GameWorld/GameWorld.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameWorld : MonoBehaviour {
+		
+	// Update is called once per frame
+	void Update () {
+
+		int pigsAmount = 0;
+		Transform blocks = transform.Find("Blocks");
+
+		foreach(Transform block in blocks)
+		{
+			if(block.GetComponent<Pig>() != null)
+				pigsAmount++;
+		}
+
+		if(pigsAmount == 0)
+		{
+			Application.LoadLevel(Application.loadedLevel);
+			return;
+		}
+	}
+}
