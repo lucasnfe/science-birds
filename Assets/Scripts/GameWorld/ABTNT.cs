b7030359cf64e536b3e228f8847279701a80a274
Lucas N. Ferreira <lucasnfe@gmail.com>
2017-02-23 19:17:17 -0800

    Fixed bug of multiple explosives together

--- Assets/Scripts/GameWorld/ABTNT.cs
+++ Assets/Scripts/GameWorld/ABTNT.cs
@@ -6,13 +6,17 @@ public class ABTNT : ABGameObject {
 	public float _explosionArea = 1f;
 	public float _explosionPower = 1f;
 	public float _explosionDamage = 1f;
+	private bool _exploded = false;
 
 	public override void Die(bool withEffect = true)
-	{
+	{		
 		//ScoreHud.Instance.SpawnScorePoint(200, transform.position);
-		Explode (transform.position, _explosionArea, _explosionPower, _explosionDamage, gameObject);
+		if (!_exploded) {
+			_exploded = true;
+			Explode (transform.position, _explosionArea, _explosionPower, _explosionDamage, gameObject);
+		}
 
-		base.Die(withEffect);
+		base.Die (withEffect);
 	}
 
 	public static void Explode(Vector2 position, float explosionArea, float explosionPower, float explosionDamage, GameObject explosive) {
@@ -31,10 +35,6 @@ public class ABTNT : ABGameObject {
 					coll.gameObject.GetComponent<ABGameObject> ().DealDamage (explosionDamage/distance);
 				
 				coll.attachedRigidbody.AddForce (direction * (explosionPower / (distance * 2f)), ForceMode2D.Impulse);
-
-				Color c = Color.red;
-				c.r /= distance;
-				coll.GetComponent<SpriteRenderer> ().color = c;
 			}
 		}
 
